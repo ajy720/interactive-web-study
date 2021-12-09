fcd7215dd97e0ab5e07ad35031be1a3dea7df247
Hyeonseok <ajy720@gmail.com>
2021-12-09 20:56:16 +0900

    sprite image animation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 ﻿const path = require('path')
+const withImages = require('next-images')
+
 // const widthBundelAnalyzer = require('@next/bundle-analyzer')({
 //     enabled: process.env.ANALYZE === 'true'
 // })
 
-module.exports = {
+module.exports = withImages({
     sassOptions: {
         includePaths: [path.join(__dirname, 'src', 'styles')],
     },
-}
+})
---
+++ src/pages/frame-animation/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './frame.module.scss'
+import image from './sprite_spaceship.png'
+
+
+const Index = () => {
+
+    return (
+        <div className="">
+            <h2>sprite image animation</h2>
+            <div className={styles.spaceship}>
+            </div>
+
+        </div>
+    )
+}
+
+export default Index
