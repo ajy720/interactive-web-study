2a909e762e413902f7835223060e41bfbe96a316
Hyeonseok <ajy720@gmail.com>
2021-12-15 23:16:01 +0900

    event delegation

--- src/pages/js/game/index.tsx
+++ src/pages/js/game/index.tsx
@@ -1,17 +1,29 @@
 import Image from 'next/image'
 import { MouseEvent, useEffect } from 'react'
- 
+
 export default () => {
 
     useEffect(() => {
-        let ilbunis = document.querySelectorAll(".ilbuni")
+        // let ilbunis = document.querySelectorAll(".ilbuni")
 
-        ilbunis.forEach((e: HTMLElement) => {
-            e.addEventListener("click", (ev) => {
-                (ev.target as HTMLElement).remove()
-            }) 
-        });
-        
+        // ilbunis.forEach((e: HTMLElement) => {
+        //     e.addEventListener("click", (ev) => {
+        //         (ev.target as HTMLElement).remove()
+        //     })
+        // });
+
+        // 이벤트 위임
+        // 이벤트 리스너를 많이 붙이는 것은 성능 저하에 직접접인 영향을 미침
+        // 따라서 리스너를 최소화하는 것이 중요. 그래서 이벤트 위임을 이용한다.
+        let stage = document.querySelector(".stage")
+
+        stage.addEventListener("click", (e) => {
+            let target = (e.target as HTMLElement);
+
+            if (target.classList.contains("ilbuni")) {
+                stage.removeChild(target)
+            }
+        })
         return () => {
 
         }
